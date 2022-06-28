2574103a7f91ed6fb52dd88659fabb384d50c765
Ligia Pretel Eimantas <ligia.pretel@yahoo.com.br>
2022-06-28 15:14:09 -0300

    Corrigindo bugs de css n apágina de login

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-function mostrarOcultarSenha () {
-    let senha = document.getElementById("password");
-    if (senha.type =="password"){
-        senha.type ="text";
-
-    } else {
-        senha.type ="password";
-    }
-}
