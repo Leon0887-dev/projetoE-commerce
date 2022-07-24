3ad44936b6402bdae59a05402427dd85d37fbfac
Ligia Pretel Eimantas <ligia.pretel@yahoo.com.br>
2022-07-24 18:14:58 -0300

    Criando página not-found

--- server/app.js
+++ server/app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
-// Importação do pacote express-session
-const session = require('express-session');
-
-//method-override.para usar PUT, DELETE no form
+const path = require("path");
 const methodOverride = require("method-override");
+const session = require('express-session');
+const cookieParser = require("cookie-parser");
 
 const indexRoute = require("./src/routes/indexRoute");
 const productRoute = require("./src/routes/productRoute");
@@ -21,18 +19,26 @@ const contactRoute = require("./src/routes/contactRoute");
 
 
 // Configurando pasta estática para acesso externo (onde ficam as imagens e css)
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join(__dirname, "public")));
 // Configurando a view engine para ejs
 app.set("view engine", "ejs");
 // indicando o caminho das nossas views
-app.set("views", __dirname + "/src/views");
+app.set("views", path.join(__dirname, "src", "views"));
 //Configurando o methodOverride no express
 app.use(methodOverride("_method"));
 // Convertendo corpo da requisição (body) em objeto literal
 app.use(express.json());
 //url encoded serve para a gente converter a carga da requisição em um formato que o json aceite
 app.use(express.urlencoded({ extended: false }));
-
+// Inicializando cookieParser
+app.use(cookieParser());
+// configuração do express-session
+app.use(session({
+    secret: 'Café House',
+    resave: false,
+    saveUninitialized: true,
+    }));
+    
 app.use("/produtos", productRoute);
 app.use("/checkout", checkoutRoute);
 app.use("/login", loginRoute);
@@ -44,12 +50,11 @@ app.use("/carrinho", carrinhoRoute);
 app.use("/contato", contactRoute);
 app.use("/administradorprodutos", administratorproductsRoute);
 
-// configuração do express-session
-app.use(session({
-    secret: 'Café House',
-    resave: false,
-    saveUninitialized: true,
-    }));
+
+app.use((req,res)=>{
+    return res.status(404).render("notFound", {title: "Página não encontrada"});
+  });
+
 
 app.listen(port, () => {
     console.log(`Estamos rodando na porta ${port}: http://localhost:3000/ `)
