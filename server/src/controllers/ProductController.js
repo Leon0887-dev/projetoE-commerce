2f0be9c6e7b8da2e2d7349fbfc2e3e6060b98ce9
Ligia Pretel Eimantas <ligia.pretel@yahoo.com.br>
2022-07-27 17:10:25 -0300

    Exibindo página de detalhe do produto através do id + criação da view error

--- server/src/controllers/ProductController.js
+++ server/src/controllers/ProductController.js
@@ -1,9 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+const productsJson = path.join("database","products.json");
+
 const productController = {
     index: (req,res)=>{
         return res.render("productListing",{title:"Produtos"});
     },
     show: (req,res)=>{
-        return res.render("product",{title:"Produto"});
+
+        //Pegando o id que virá via url - GET
+        const {id} = req.params;
+
+        // Lendo arquivo json
+        let products = fs.readFileSync(productsJson,{enconding:'utf-8'})
+        // Transformando o formato JSON em um array novamente
+        products=JSON.parse(products);
+
+        //Verificando se o id existe
+        const productResult = products.find((product)=>product.id===parseInt(id));
+        // Se não existir, renderiza a view error mostrando a mensagem 
+        if(!productResult){
+            return res.render("error",{
+                title:"Ops!",
+                message:"Produto não encontrado."
+            });
+        };
+        // Caso contrário, copiamos as informações de productResult usando spread operator para a variável product
+        const product = {
+            ...productResult,
+        };
+
+        // Por fim, retorno a view passando as informações do produto
+        return res.render("product",{
+            title:"Detalhe do Produto",
+            product,
+        });
     },
 };
 
--- server/src/routes/productRoute.js
+++ server/src/routes/productRoute.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const productController = require("../controllers/ProductController");
 
 router.get("/",productController.index);
-router.get("/detalheProduto",productController.show);
+router.get("/detalheProduto/:id",productController.show);
 
 module.exports = router;
