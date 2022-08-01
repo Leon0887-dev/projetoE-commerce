4450b36f871e4ba0756036c27aafe5eb0084b2ac
leon guilherme <leonlopes1106@gmail.com>
2022-08-01 18:00:10 -0300

    tirando camelcase das rotas do painel usuário

--- server/src/routes/userPanelRoute.js
+++ server/src/routes/userPanelRoute.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const userPanelController = require("../controllers/UserPanelController");
 
 router.get("/",userPanelController.index);
-router.get("/meusPedidos",userPanelController.myRequests);
-router.get("/meusEnderecos",userPanelController.myAddresses);
-router.get("/meusDados",userPanelController.myPersonalData);
-router.get("/alterarSenha",userPanelController.changePassword);
+router.get("/meuspedidos",userPanelController.myRequests);
+router.get("/meusenderecos",userPanelController.myAddresses);
+router.get("/meusdados",userPanelController.myPersonalData);
+router.get("/alterarsenha",userPanelController.changePassword);
 
 module.exports = router;
