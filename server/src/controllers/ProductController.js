5acbdb86862a387325f6119d3b9f5d8339a3b31f
Ligia Pretel Eimantas <ligia.pretel@yahoo.com.br>
2022-09-22 21:46:25 -0300

    Fazendo os filtros - categoria e marca - na página productListing

--- server/src/controllers/ProductController.js
+++ server/src/controllers/ProductController.js
@@ -12,76 +12,73 @@ const ProductImage = require("../models/ProductImage");
 const productController = {
     index: async (req, res) => {
 
-        try{
+        try {
 
             //Armazenando chave da query string
             const category = req.query.categoria;
+            const brand = req.query.marca;
 
             //Recebendo a paginação via query string, se não vier página, por padrão será 1
-            let { pagina=1 } = req.query; 
-                        
-            const {count: total, rows: products} = await Product.findAndCountAll({
+            let { pagina = 1 } = req.query;
+
+            const include = [{
+                model: ImageProduct
+            }]
+
+            if(category){
+                include.push({
+                    model: Category,
+                        where: {
+                            name: category,
+                        }
+                })
+            }
+
+            if(brand){
+                include.push({
+                    model: Brand,
+                        where: {
+                            name: brand,
+                        }
+                })
+            }
+
+            const { count: total, rows: products } = await Product.findAndCountAll({
                 where: {
                     active: 1,
                 },
                 limit: 8,
-                offset: (pagina-1)*8,
-                include: ImageProduct,
+                offset: (pagina - 1) * 8,
+                include,
             });
-            
+
             //Math.ceil() - arredonda um número para cima, para o próximo valor inteiro - Ex.: 1.3 -> 2 
-            let totalPages = Math.ceil(total/8);
+            let totalPages = Math.ceil(total / 8);
 
             const brands = await Brand.findAll({
                 order: [
-                    ["name","ASC"],
+                    ["name", "ASC"],
                 ]
             });
 
             const categories = await Category.findAll({
                 order: [
-                    ["name","ASC"],
+                    ["name", "ASC"],
                 ]
             });
-    
-            //Verificando se existe alguma categoria
-            if (category) {
 
-                const productResult = await Product.findAll({
-                    where: {
-                        active: 1,
-                    },
-                    include: [
-                        { model: ImageProduct },
-                        { model: Category,
-                            where: {
-                                name: category,
-                            }
-                        },
-                    ],
-                });
-    
-                //Renderizando a página com os produtos da categoria
-                return res.render("productListing", {
-                    title: "Produtos",
-                    products: productResult,
-                    brands,
-                    categories,
-                    totalPages,
-                    user: req.cookies.user            
-                });
-            }
-    
+            console.log(products);
+
             return res.render("productListing", {
                 title: "Produtos",
                 products,
                 brands,
                 categories,
                 totalPages,
-                user: req.cookies.user            
+                user: req.cookies.user
             });
 
-        }catch(error){
+        } catch (error) {
             console.log(error);
             return res.render("error", {
                 title: "Ops!",
@@ -93,7 +90,9 @@ const productController = {
     show: (req, res) => {
 
         //Pegando o id que virá via url - GET
-        const {id} = req.params;
+        const {
+            id
+        } = req.params;
 
         // Lendo arquivo json
         let products = fs.readFileSync(productsJson, {
@@ -109,7 +108,7 @@ const productController = {
             return res.render("error", {
                 title: "Ops!",
                 message: "Produto não encontrado.",
-                user: req.cookies.user            
+                user: req.cookies.user
             });
         };
         // Caso contrário, copiamos as informações de productResult usando spread operator para a variável product
@@ -121,7 +120,7 @@ const productController = {
         return res.render("product", {
             title: "Detalhe do Produto",
             product,
-            user: req.cookies.user            
+            user: req.cookies.user
         });
     },
 };
