74118da068aa6469425a409c4e64fd40c620a1e2
Bruno Sanches <bsan.designgrafico@gmail.com>
2022-09-27 21:18:46 -0300

    Auth banco de dados ok

--- server/src/routes/authRoute.js
+++ server/src/routes/authRoute.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/AuthController");
+const adminAuth = require("../controllers/admin/AuthController");
 const isAuth = require("../middlewares/auth");
 const isGuest = require("../middlewares/guest");
 
 router.get("/login", isGuest, authController.login);
-router.post("/login", isGuest, authController.auth);
+router.post("/login", isGuest, adminAuth.auth);
 
 router.get("/registro", isGuest, authController.register);
-router.post("/registro", isGuest, authController.create);
+router.post("/registro", isGuest, adminAuth.store);
 
-router.post("/logout", isAuth, authController.logout);
+router.post("/logout", isAuth, adminAuth.logout);
 
 module.exports = router;
